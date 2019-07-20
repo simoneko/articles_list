f94c2742dfcd33761c1dcc674f7746b07888df88
Szymek <mail@m.pl>
2019-07-20 21:06:44 +0200

    Updates

---
+++ precache-manifest.91746dcdd9c4e16b80e49b1b4b5a9517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f8cf2384a7548b3b58d57bd33b25c6",
+    "url": "/articles_list/index.html"
+  },
+  {
+    "revision": "8de81c02a5c2f9ea0622",
+    "url": "/articles_list/static/css/main.4b866b88.chunk.css"
+  },
+  {
+    "revision": "a1f817bf7c49c18c1b5b",
+    "url": "/articles_list/static/js/2.c5107086.chunk.js"
+  },
+  {
+    "revision": "8de81c02a5c2f9ea0622",
+    "url": "/articles_list/static/js/main.2a9bcfa8.chunk.js"
+  },
+  {
+    "revision": "ef9c748816c1778aa0b8",
+    "url": "/articles_list/static/js/runtime~main.505f0c25.js"
+  }
+]);
