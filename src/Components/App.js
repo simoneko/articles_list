372c2f8ed0d7d03f00cabec3b604952928e7c3e2
Szymek <mail@m.pl>
2019-07-20 21:21:33 +0200

    Remove comments

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,56 +1,7 @@
 import React from 'react';
 import '../styles/App.css';
 import ArticlesList from './ArticlesList';
-// import ApolloClient, { gql } from "apollo-boost";
-// import { ApolloProvider, Query } from 'react-apollo';
-
 import axios from 'axios';
-
-// const client = new ApolloClient({
-//   uri: "https://mobileapi.wp.pl/v1/graphql"
-// });
-
-// client.query({
-//   query: gql`
-//       {
-//         articles(t: Article, limit: 10) {
-//           id
-//           title
-//           body {
-//             data
-//           }
-//         }
-//       }`
-// }).then(result => console.log(result.data.articles));
-
-// const ExchangeRates = () => (
-//   <Query
-//     query={gql`
-//     {
-//       articles(t: Article, limit: 10) {
-//         id
-//         title
-//         body {
-//           data
-//         }
-//       }
-//     }`
-//     }
-//   >
-//     {({ loading, error, data }) => {
-//       if (loading) return <p>Loading...</p>;
-//       if (error) return <p>Error!</p>;
-
-//       return data.articles.map(({ id, title }) => (
-//         <div key={id} >
-//           <p>{title}</p>
-//         </div>
-//       ))
-//     }}
-
-//   </Query>
-// )
-
 
 
 class App extends React.Component {
@@ -98,22 +49,6 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    // client.query({
-    //   query: gql`
-    //       {
-    //         articles(t: Article, limit: 10) {
-    //           id
-    //           title
-    //           body {
-    //             data
-    //           }
-    //         }
-    //       }` 
-    // }).then(result => {
-    //   this.setState({
-    //     list: result.data.articles
-    //   })
-    // })
 
     const query =
       `
